78762814d23be3f41dd91da839ba51aa66a3036a
parazzit213 <small213.vb@gmail.com>
2024-12-15 22:58:21 +0300

    Delete psychological_bot directory

--- psychological_bot/utils/localization.py
+++
@@ -1,7 +0,0 @@
-user_preferences = {}  # Простая реализация, для продакшена использовать базу данных
-
-def set_language(user_id: int, language: str):
-    user_preferences[user_id] = {'language': language}
-
-def get_language(user_id: int):
-    return user_preferences.get(user_id, {}).get('language', 'en')
--- psychological_bot/utils/notifications.py
+++
@@ -1,2 +0,0 @@
-async def send_notification(bot, user_id, message):
-    await bot.send_message(user_id, message)
